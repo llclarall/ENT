e941560f7da838e78c9fd1c1dd64b5b6bee91c77
Melll03 <melissa.cmr03@gmail.com>
2024-12-17 09:54:18 +0100

    ajout img + style

--- elearning/elearning.php
+++ elearning/elearning.php
@@ -15,29 +15,20 @@
     <a href="archives.php" class="archives-btn">Archives des cours</a>
 
     <!-- Création numérique Section -->
+     <div class="elearning-container">
     <div class="section-title">Création numérique</div>
     <div class="course-container">
       <div class="course-card">
+        <img src="img/creation.png" alt="">
         <div class="course-details">
           <strong>R3.09 Création et Design interactif MMI2 - S3</strong>
           <span>Eiffel Année universitaire 2024/2025</span>
         </div>
       </div>
       <div class="course-card">
+      <img src="img/audiovisuel.jpg" alt="">
       <div class="course-details">
-          <strong>R3.09 Création et Design interactif MMI2 - S3</strong>
-          <span>Eiffel Année universitaire 2024/2025</span>
-        </div>
-      </div>
-      <div class="course-card">
-      <div class="course-details">
-          <strong>R3.09 Création et Design interactif MMI2 - S3</strong>
-          <span>Eiffel Année universitaire 2024/2025</span>
-        </div>
-      </div>
-      <div class="course-card">
-      <div class="course-details">
-          <strong>R3.09 Création et Design interactif MMI2 - S3</strong>
+          <strong>Audiovisuel & Motion Design</strong>
           <span>Eiffel Année universitaire 2024/2025</span>
         </div>
       </div>
@@ -46,35 +37,27 @@
     <div class="section-title">Développement web</div>
     <div class="course-container">
       <div class="course-card">
+        <img src="img/deploiement.jpg" alt="">
         <div class="course-details">
           <strong>Déploiement de services MMI2 2024</strong>
           <span>Eiffel Année universitaire 2024/2025</span>
         </div>
       </div>
       <div class="course-card">
+        <img src="img/javascript.jpg" alt="">
       <div class="course-details">
-          <strong>Déploiement de services MMI2 2024</strong>
+          <strong>R3.12 Développement Javascript</strong>
           <span>Eiffel Année universitaire 2024/2025</span>
         </div>
       </div>
       <div class="course-card">
+        <img src="img/integration.jpg" alt="">
       <div class="course-details">
-          <strong>Déploiement de services MMI2 2024</strong>
+          <strong>Intégration web - BUT MMI2</strong>
           <span>Eiffel Année universitaire 2024/2025</span>
         </div>
-      </div>
-      <div class="course-card">
-      <div class="course-details">
-          <strong>Déploiement de services MMI2 2024</strong>
-          <span>Eiffel Année universitaire 2024/2025</span>
-        </div>
-      </div>
-      <div class="course-card">
-      <div class="course-details">
-          <strong>Déploiement de services MMI2 2024</strong>
-          <span>Eiffel Année universitaire 2024/2025</span>
-        </div>
-      </div>
+      </div>     
+    </div>
     </div>
   </main>  
 </body>
