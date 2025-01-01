839d3e7ea3dc4cad430e7e10d8e79ee4de79c8cb
Melll03 <melissa.cmr03@gmail.com>
2025-01-01 20:31:42 +0100

    Creation page vie etudiante

---
+++ elearning/vie_etudiante.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Vie étudiante</title>
+</head>
+<body>
+<div class="page-container">
+<h1>Vie étudiante</h1>
+
+<div class="sections-container">
+  <section class="section-wrapper">
+    <h2>Forum/FAQ</h2>
+    <div class="info-card">
+      <img src="img/forum.jpg" alt="Voyage en Irlande">
+      <div class="info-card-content">
+        <div class="info-card-title">Partir à l'étranger</div>
+        <div class="info-card-description">Intéressé par un voyage à l'étranger ? Service des Relations Internationale vous accompagne pour préparer votre projet.</div>
+        <a href="#" class="info-card-button">En savoir plus</a>
+      </div>
+    </div>
+  </section>
+
+      <section class="section-wrapper large-card" >
+        <h2>Événements - UGE</h2>
+        <div class="info-card">
+          <img src="img/evenement.png" alt="Salon de l'alternance">
+          <div class="info-card-content">
+            <div class="info-card-title">Élection des représentants</div>
+            <div class="info-card-description">C'est avec enthousiasme que l'Université Gustave Eiffel vous annonce l'ouverture des élections des représentants étudiants au sein de notre institution.</div>
+            <a href="#" class="info-card-button">En savoir plus</a>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-wrapper">
+        <h2>Offre stage / alternance</h2>
+        <div class="info-card">
+          <img src="img/stage.jpg" alt="Salon de l'alternance">
+          <div class="info-card-content">
+            <div class="info-card-title">Salon de l'alternance</div>
+            <div class="info-card-description">L'université Gustave Eiffel est heureuse de vous inviter à son Salon de l'alternance, le 3 décembre.</div>
+            <a href="#" class="info-card-button">En savoir plus</a>
+          </div>
+        </div>
+      </section>
+
+
+      <section class="section-wrapper large-card">
+        <h2>Internationale</h2>
+        <div class="info-card">
+          <img src="img/voyage.png" alt="Voyage en Irlande">
+          <div class="info-card-content">
+            <div class="info-card-title">Voyage en Irlande</div>
+            <div class="info-card-description">L'université Gustave Eiffel a le plaisir de vous annoncer l'organisation d'un voyage en Irlande.</div>
+            <a href="#" class="info-card-button">En savoir plus</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+
+</body>
+</html>
