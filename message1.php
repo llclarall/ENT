004f4fce05414ac7c1cdc080ac1bea161c5e45ca
Melll03 <melissa.cmr03@gmail.com>
2025-01-05 16:07:06 +0100

    ajout messagerie + page mess

---
+++ message1.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Détails du message</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+ <main class="message-content">
+ <a href="messagerie.php" class="back-link">
+      <button id="back-button" class="back-button">Retour</button>
+    </a>
+    <strong class="sender-name">G. Charpentier</strong>
+    <p class="email">gaelle.charpentier@univ-eiffel.fr</p>
+    <p class="date-time">10/12/2024 à 14h36</p>
+    <p class="message-text">Le prochain QCM sera le 29 novembre 2024</p>
+    <div class="actions">
+      <button class="action-button">Répondre</button>
+      <button class="action-button">Transférer</button>
+    </div>
+  </main>
+</body>
+</html>
