624a67c3d3696188f03ef092aa3bda4af6c70f5c
Melll03 <melissa.cmr03@gmail.com>
2025-01-05 17:41:06 +0100

    ajout icon messagerie

--- messagerie.php
+++ messagerie.php
@@ -8,6 +8,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <title>Messagerie</title>
   <link rel="stylesheet" href="styles.css">
 </head>
@@ -16,8 +17,8 @@
     <div class="sidebar">
   <button class="active">Boîte de réception</button>
   <button>Nouveau message</button>
-  <button>Envoyés</button>
-  <button>Archivés</button>
+  <button><i class="fas fa-paper-plane"></i> Envoyés</button>
+  <button><i class="fas fa-archive"></i> Archivés</button>
 </div>
 
     <div class="messages">
