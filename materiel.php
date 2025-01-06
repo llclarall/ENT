596b5a9fa78f52148a6c8c149ce1a78bd2fb43ef
_clara_ <152849066+llclarall@users.noreply.github.com>
2025-01-06 16:31:02 +0100

    page réserver intégrée

---
+++ materiel.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&family=Barriecito&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matériel</title>
+</head>
+<body>
+
+<div class="matos">
+    <h1 class="matosh">Matériel</h1>
+</div>
+
+    <div class="main-container">
+        <div class="main-rectangle">
+            <h2 class="main-title">Choisissez votre matériel</h2>
+            <div class="row-container">
+                <div class="sub-block">Caméra</div>
+                <div class="sub-block">Trépied</div>
+                <div class="sub-block">Diffuseur</div>
+            </div>
+            <div class="row-container">
+                <div class="sub-block">Micro</div>
+                <div class="sub-block">LED</div>
+                <div class="sub-block">Dji OSMO</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
