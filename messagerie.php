c7b17286ff445c7c6c168f4bfee93028d7e5ebf9
_clara_ <152849066+llclarall@users.noreply.github.com>
2025-01-06 02:37:40 +0100

    messagerie intégrée

--- messagerie.php
+++ messagerie.php
@@ -3,23 +3,30 @@
     include('config.php');
     include('nav.php');
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-  <title>Messagerie</title>
+  <title>ENT | Messagerie</title>
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <div class="container-messagerie">    
+
+<section class="page-messagerie">
+
+<h1>Messagerie</h1>
+
+  <div class="container-messagerie">
+
     <div class="sidebar">
-  <button class="active">Boîte de réception</button>
-  <button>Nouveau message</button>
-  <button><i class="fas fa-paper-plane"></i> Envoyés</button>
-  <button><i class="fas fa-archive"></i> Archivés</button>
-</div>
+      <button class="active">Boîte de réception</button>
+      <button>Nouveau message</button>
+      <button><i class="fas fa-paper-plane"></i> Envoyés</button>
+      <button><i class="fas fa-archive"></i> Archivés</button>
+    </div>
 
     <div class="messages">
       <a href="message1.php"class="message-item">
@@ -31,7 +38,6 @@
           <span class="date">10/12/2024</span>
         </div>
         </a>
-
         <a href="message1.php"class="message-item">
         <div class="message-info">
           <strong>G. Charpentier</strong>
@@ -41,8 +47,6 @@
           <span class="date">10/12/2024</span>
         </div>
         </a>
-
-
         <a href="message1.php"class="message-item">
         <div class="message-info">
           <strong>G. Charpentier</strong>
@@ -51,9 +55,12 @@
         <div class="message-meta">
           <span class="date">10/12/2024</span>
         </div>
-        </a>
-
+      </a>
     </div>
+
   </div>
+
+</section>
+
 </body>
 </html>
